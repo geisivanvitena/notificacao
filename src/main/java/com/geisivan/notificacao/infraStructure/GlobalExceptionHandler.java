24b9f4b5cf9dbb1af55975001d35ae8ab07cd0d1
Geisivan <gsv1205@yahoo.com>
2026-02-22 20:39:42 -0300

    Ajustes no GlobalExceptionHandler e gitignore

--- src/main/java/com/geisivan/notificacao/infraStructure/GlobalExceptionHandler.java
+++ src/main/java/com/geisivan/notificacao/infraStructure/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.geisivan.notificacao.infraStructure;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,10 +8,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Business exceptions
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<String> handleApiException(ApiException exception) {
         return ResponseEntity
                 .status(exception.getStatus())
                 .body(exception.getMessage());
+    }
+
+    // Erros inesperados
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception exception){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Erro interno no servidor");
     }
 }
