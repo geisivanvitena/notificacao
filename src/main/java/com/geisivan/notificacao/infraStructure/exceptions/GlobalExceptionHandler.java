af880eb206d0df44634dec447af9c9c6144e0956
Geisivan <gsv1205@yahoo.com>
2026-03-06 18:52:23 -0300

    refactor: padroniza tratamento global de exceções e resposta de erro da API

---
+++ src/main/java/com/geisivan/notificacao/infraStructure/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.geisivan.notificacao.infraStructure.exceptions;
+
+import com.geisivan.notificacao.infraStructure.exceptions.dto.ErrorResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Business exceptions
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ErrorResponseDTO> handleApiException(ApiException exception,
+                                                     HttpServletRequest request) {
+        ErrorResponseDTO error = buildError(
+                exception.getStatus(),
+                exception.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity
+                .status(exception.getStatus())
+                .body(error);
+    }
+
+    // Erros inesperados
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleGenericException(Exception exception,
+                                                         HttpServletRequest request){
+        ErrorResponseDTO error = buildError(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Erro interno no servidor",
+                request.getRequestURI()
+        );
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(error);
+    }
+
+    private ErrorResponseDTO buildError(HttpStatus status, String message, String path) {
+        return ErrorResponseDTO.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message(message)
+                .path(path)
+                .build();
+    }
+}
